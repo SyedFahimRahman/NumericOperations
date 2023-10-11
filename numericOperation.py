47d4e149a79ec6e692fa016277aacbb9058a6fd3
ULStudent:SYED FAHIM.RAHMAN <23376066@studentmail.ul.ie>
2023-10-11 12:18:26 +0100

    added source and test files

--- numericOperation.py
+++ numericOperation.py
@@ -1,3 +1,7 @@
 
 def multiply_op(n1,n2):
 	return n1*n2
+def addition_op(n1,n2):
+	return n1+n2
+def division_op(n1,n1):
+	return n1/n2
--- testing.py
+++ testing.py
@@ -1,6 +1,14 @@
 from numericOperation import multiply_op
+from numericOperation import addition-op
+from numericOperation import division_op
+
 
 
 
 def test_mult():
 	assert multiply_op(10,11) == 110
+def test_add():
+	assert addition_op(10,11) == 21
+def test_mult():
+	assert division_op(10,2) == 5
+
